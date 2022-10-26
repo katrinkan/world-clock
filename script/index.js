7f24ca1ec1cca146bd7c647855b9c45032232ccf
katrinkan <96773609+katrinkan@users.noreply.github.com>
2022-10-26 13:49:24 +0200

    added current location time and date

--- script/index.js
+++ script/index.js
@@ -1,32 +1,41 @@
+const barcelonaElement = document.querySelector("#barcelona");
+const hongKongElement = document.querySelector("#hong-kong");
+
 function updateTime() {
   // Barcelona
-
-  const barcelonaElement = document.querySelector("#barcelona");
-  const barcelonaDateElement = barcelonaElement.querySelector(".date");
-  const barcelonaTimeElement = barcelonaElement.querySelector(".time");
-  const barcelonaTime = moment().tz("Europe/Madrid");
-  barcelonaDateElement.innerHTML = moment().format("MMMM Do YYYY");
-  barcelonaTimeElement.innerHTML = `${barcelonaTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  )}`;
+  if (barcelonaElement) {
+    const barcelonaDateElement = barcelonaElement.querySelector(".date");
+    const barcelonaTimeElement = barcelonaElement.querySelector(".time");
+    const barcelonaTime = moment().tz("Europe/Madrid");
+    barcelonaDateElement.innerHTML = moment().format("MMMM Do YYYY");
+    barcelonaTimeElement.innerHTML = `${barcelonaTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
 
   //Hong Kong
-
-  const hongKongElement = document.querySelector("#hong-kong");
-  const hongKongDateElement = hongKongElement.querySelector(".date");
-  const hongKongTimeElement = hongKongElement.querySelector(".time");
-  const hongKongTime = moment().tz("Asia/Hong_Kong");
-  hongKongDateElement.innerHTML = moment().format("MMMM Do YYYY");
-  hongKongTimeElement.innerHTML = `${hongKongTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  )}`;
+  if (hongKongElement) {
+    const hongKongDateElement = hongKongElement.querySelector(".date");
+    const hongKongTimeElement = hongKongElement.querySelector(".time");
+    const hongKongTime = moment().tz("Asia/Hong_Kong");
+    hongKongDateElement.innerHTML = moment().format("MMMM Do YYYY");
+    hongKongTimeElement.innerHTML = `${hongKongTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    )}`;
+  }
 }
-updateTime();
-setInterval(updateTime, 1000);
+if (barcelonaElement && hongKongElement) {
+  updateTime();
+  setInterval(updateTime, 1000);
+}
 
 function updateCity(event) {
   function displayNewTime() {
-    const cityTimeZone = event.target.value;
+    let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+      cityTimeZone = moment.tz.guess();
+      console.log(cityTimeZone);
+    }
     const cityName = cityTimeZone.replace("_", " ").split("/")[1];
     const cityTime = moment().tz(cityTimeZone);
     const citiesElement = document.querySelector("#cities");
