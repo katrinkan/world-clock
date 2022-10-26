441ec24ce9ec54f714ff83a446c9942bd4c8d493
katrinkan <96773609+katrinkan@users.noreply.github.com>
2022-10-26 13:36:49 +0200

    added time and date for multiple locations

--- script/index.js
+++ script/index.js
@@ -1,5 +1,6 @@
 function updateTime() {
   // Barcelona
+
   const barcelonaElement = document.querySelector("#barcelona");
   const barcelonaDateElement = barcelonaElement.querySelector(".date");
   const barcelonaTimeElement = barcelonaElement.querySelector(".time");
@@ -10,6 +11,7 @@ function updateTime() {
   )}`;
 
   //Hong Kong
+
   const hongKongElement = document.querySelector("#hong-kong");
   const hongKongDateElement = hongKongElement.querySelector(".date");
   const hongKongTimeElement = hongKongElement.querySelector(".time");
@@ -21,3 +23,26 @@ function updateTime() {
 }
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateCity(event) {
+  function displayNewTime() {
+    const cityTimeZone = event.target.value;
+    const cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    const cityTime = moment().tz(cityTimeZone);
+    const citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `<div class="city" id="hong-kong">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss [<small>]A[</small>]"
+          )}</div>
+        </div>`;
+  }
+  displayNewTime();
+  setInterval(displayNewTime, 1000);
+}
+
+const citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
